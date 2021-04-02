a79ae835d4a3c15649f0ec1c00169baca1bdc4a0
TSFelg <38079600+TSFelg@users.noreply.github.com>
2021-04-02 11:26:32 +0100

    add setup.sh to Procfile and fix setup.sh

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh streamlit run app.py
+web: sh setup.sh && streamlit run app.py
--- setup.sh
+++ setup.sh
@@ -7,7 +7,11 @@ email = \"your-email@domain.com\"\n\
 
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor='#6eb52f'\n\
+backgroundColor='#f0f0f5'
+secondaryBackgroundColor='#e0e0ef'
+textColor='#262730'
+font='sans serif'
 " > ~/.streamlit/config.toml
